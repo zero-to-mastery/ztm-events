a7422019b138e5f138385d718b9213a01195618c
DESKTOP-B7BGNT1\Admin <anandapadmanabhanvt@gmail.com>
2023-09-16 11:26:12 +0530

    Added Search fieild and styles to logo and navigation

--- app/components/navigation.jsx
+++ app/components/navigation.jsx
@@ -1,10 +1,16 @@
-import Logo from './logo'
-
+import Logo from './logo';
+import SearchLocation from './searchLocation';
 
 export default function Navigation() {
-    return (
-        <>
-        <div className='h-24' style={{backgroundColor: 'rgb(246, 244, 210)'}}><Logo /></div>
-        </>
-    )
+  return (
+    <>
+      <div
+        className='h-24 flex justify-between items-center w-full px-1'
+        style={{ backgroundColor: 'rgb(246, 244, 210)' }}
+      >
+        <Logo />
+        <SearchLocation />
+      </div>
+    </>
+  );
 }
