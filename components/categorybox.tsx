9c68ef3858b789d52297a283b4f602f8edab3ef0
DESKTOP-B7BGNT1\Admin <anandapadmanabhanvt@gmail.com>
2023-10-08 11:52:16 +0530

    ts arrangement and prop fix

--- components/categorybox.tsx
+++ components/categorybox.tsx
@@ -1,21 +1,27 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import { ZtmEventCategory } from './categories';
 
-const CategoryBox = ({ ztmevent }) => {
-    
-    return (
-        <div className="bg-white rounded-lg shadow-lg">
-        <Link href={`/${ztmevent.categoryName}`}>
+interface ZtmEventCategoryBoxProps {
+  id: number;
+  ztmevent: ZtmEventCategory;
+}
+
+const CategoryBox = (ztmeventProps: ZtmEventCategoryBoxProps) => {
+  const { ztmevent } = ztmeventProps;
+  return (
+    <div className='bg-white rounded-lg shadow-lg'>
+      <Link href={`/${ztmeventProps.id}`}>
         <img
-            src={ztmevent.categoryImage}
-            alt={ztmevent.categoryName}
-            className="w-full h-40 object-cover rounded-t-lg"
+          src={ztmevent.categoryImage}
+          alt={ztmevent.categoryName}
+          className='w-full h-40 object-cover rounded-t-lg'
         />
-        <div className="p-4">
-            <h3 className="text-lg font-semibold">{ztmevent.categoryName}</h3>
+        <div className='p-4'>
+          <h3 className='text-lg font-semibold'>{ztmevent.categoryName}</h3>
         </div>
-        </Link>
+      </Link>
     </div>
-    )
-  }
-  
-  export default CategoryBox
+  );
+};
+
+export default CategoryBox;
